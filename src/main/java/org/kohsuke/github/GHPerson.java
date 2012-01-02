79774714583afe3fd390259f1e34634e9b1d153a
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-01 17:43:44 -0800

    Pulled up more properties to the base type

--- src/main/java/org/kohsuke/github/GHPerson.java
+++ src/main/java/org/kohsuke/github/GHPerson.java
@@ -14,11 +14,17 @@ import java.util.TreeMap;
 public abstract class GHPerson {
     /*package almost final*/ GitHub root;
 
-    protected String gravatar_id,login;
+    // common
+    protected String login,location,blog,email,name,created_at,company;
+    protected int id;
+    protected String gravatar_id; // appears in V3 as well but presumably subsumed by avatar_url?
 
-    protected int public_gist_count,public_repo_count,following_count,id;
-    
-    protected String avatar_url;
+    // V2
+    protected int public_gist_count,public_repo_count,followers_count,following_count;
+
+    // V3
+    protected String avatar_url,html_url;
+    protected int followers,following,public_repos,public_gists;
 
     /**
      * Gets the repositories this user owns.
@@ -54,7 +60,73 @@ public abstract class GHPerson {
         return gravatar_id;
     }
 
+    /**
+     * Gets the login ID of this user, like 'kohsuke'
+     */
     public String getLogin() {
         return login;
     }
+
+    /**
+     * Gets the human-readable name of the user, like "Kohsuke Kawaguchi"
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the company name of this user, like "Sun Microsystems, Inc."
+     */
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     * Gets the location of this user, like "Santa Clara, California"
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCreatedAt() {
+        return created_at;
+    }
+
+    /**
+     * Gets the blog URL of this user.
+     */
+    public String getBlog() {
+        return blog;
+    }
+
+    /**
+     * Gets the e-mail address of the user.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPublicGistCount() {
+        return Math.max(public_gist_count,public_gists);
+    }
+
+    public int getPublicRepoCount() {
+        return Math.max(public_repo_count,public_repos);
+    }
+
+    public int getFollowingCount() {
+        return Math.max(following_count,following);
+    }
+
+    /**
+     * What appears to be a GitHub internal unique number that identifies this user.
+     */
+    public int getId() {
+        return id;
+    }
+
+    public int getFollowersCount() {
+        return Math.max(followers_count,followers);
+    }
+
 }

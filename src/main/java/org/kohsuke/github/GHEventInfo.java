696daf73875f29f87c693fe73c4e2d77ca614c82
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-01-08 17:23:28 -0800

    adding the payload handling

--- src/main/java/org/kohsuke/github/GHEventInfo.java
+++ src/main/java/org/kohsuke/github/GHEventInfo.java
@@ -1,9 +1,16 @@
 package org.kohsuke.github;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.type.TypeFactory;
 import org.codehaus.jackson.node.ObjectNode;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 /**
  * Represents an event.
@@ -13,6 +20,7 @@ import java.util.Date;
 public class GHEventInfo {
     private GitHub root;
 
+    // we don't want to expose Jackson dependency to the user. This needs databinding
     private ObjectNode payload;
 
     private String created_at;
@@ -24,8 +32,10 @@ public class GHEventInfo {
     private GHOrganization org;
     
     public GHEvent getType() {
+        String t = type;
+        if (t.endsWith("Event"))    t=t.substring(0,t.length()-5);
         for (GHEvent e : GHEvent.values()) {
-            if (e.name().replace("_","").equalsIgnoreCase(type))
+            if (e.name().replace("_","").equalsIgnoreCase(t))
                 return e;
         }
         return null;    // unknown event type
@@ -53,5 +63,50 @@ public class GHEventInfo {
 
     public GHOrganization getOrganization() throws IOException {
         return (org==null || org.getLogin()==null) ? null : root.getOrganization(org.getLogin());
+    }
+
+    /**
+     * Retrieves the payload.
+     * 
+     * @param type
+     *      Specify one of the {@link Payload} subtype that defines a type-safe access to the payload.
+     *      This must match the {@linkplain #getType() event type}.
+     */
+    public <T extends Payload> T getPayload(Class<T> type) {
+        return type.cast(Proxy.newProxyInstance(getClass().getClassLoader(), new Class<?>[]{type}, new InvocationHandler() {
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (method.getDeclaringClass() == Object.class)
+                    return method.invoke(this, method, args); // hashCode, equals
+
+                String name = method.getName();
+                if (name.startsWith("get")) name = name.substring(3);
+
+                Iterator<Entry<String,JsonNode>> itr = payload.getFields();
+                while (itr.hasNext()) {
+                    Entry<String, JsonNode> e = itr.next();
+                    if (e.getKey().replace("_","").equalsIgnoreCase(name)) {
+                        return GitHub.MAPPER.readValue(e.getValue().traverse(),
+                                TypeFactory.type(method.getGenericReturnType()));
+                    }
+                }
+                
+                return null;
+            }
+        }));
+    }
+
+    /**
+     * Marker interface for types used for databinding of the event payload.
+     */
+    public interface Payload {}
+
+    public interface CommitComment extends Payload {
+        // GHComment getComment();
+    }
+
+    public interface PullRequest extends Payload {
+        String getAction();
+        int getNumber();
+        GHPullRequest getPullRequest();
     }
 }

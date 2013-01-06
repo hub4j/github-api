6480dde24748fa7b1ee362b6cb8e08c25e77caac
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-01-05 17:15:46 -0800

    oops test failures

--- src/test/java/org/kohsuke/github/GitHubTest.java
+++ src/test/java/org/kohsuke/github/GitHubTest.java
@@ -8,17 +8,12 @@ import junit.framework.TestCase;
 public class GitHubTest extends TestCase {
 
     public void testGitHubServerWithHttp() throws Exception {
-        GitHub hub = GitHub.connect("http://enterprise.kohsuke.org/api/v3", "kohsuke", "token", "password");
+        GitHub hub = GitHub.connectToEnterprise("http://enterprise.kohsuke.org/api/v3", "kohsuke", "token");
         assertEquals("http://enterprise.kohsuke.org/api/v3/test", hub.getApiURL("/test").toString());
     }
 
     public void testGitHubServerWithHttps() throws Exception {
-        GitHub hub = GitHub.connect("https://enterprise.kohsuke.org/api/v3", "kohsuke", "token", "password");
-        assertEquals("https://enterprise.kohsuke.org/api/v3/test", hub.getApiURL("/test").toString());
-    }
-
-    public void testGitHubServerWithoutProtocol() throws Exception {
-        GitHub hub = GitHub.connect("enterprise.kohsuke.org", "kohsuke", "token", "password");
+        GitHub hub = GitHub.connectToEnterprise("https://enterprise.kohsuke.org/api/v3", "kohsuke", "token");
         assertEquals("https://enterprise.kohsuke.org/api/v3/test", hub.getApiURL("/test").toString());
     }
 

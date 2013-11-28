f3207855cad72e91f2a8ed553afdc02aa1934d54
Matt Farmer <matt@frmr.me>
2013-11-28 09:45:54 -0500

    Implement a CRUD integration test for the content api.

--- src/test/java/org/kohsuke/github/GHContentIntegrationTest.java
+++ src/test/java/org/kohsuke/github/GHContentIntegrationTest.java
@@ -3,21 +3,24 @@ package org.kohsuke.github;
 import junit.framework.TestCase;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
- * Unit test for {@link GHContent}.
+ * Integration test for {@link GHContent}.
  */
 public class GHContentIntegrationTest extends TestCase {
 
     private GitHub gitHub;
     private GHRepository repo;
+    private String createdFilename;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        // we just read at the moment
-        gitHub = GitHub.connectAnonymously();
-        repo = gitHub.getUser("acollign").getRepository("github-api-test");
+
+        gitHub = GitHub.connect();
+        repo = gitHub.getRepository("acollign/github-api-test").fork();
+        createdFilename = UUID.randomUUID().toString();
     }
 
     public void testGetFileContent() throws Exception {
@@ -38,5 +41,27 @@ public class GHContentIntegrationTest extends TestCase {
         List<GHContent> entries = repo.getDirectoryContent("ghcontent-ro/a-dir-with-3-entries");
 
         assertTrue(entries.size() == 3);
+    }
+
+    public void testCRUDContent() throws Exception {
+        GHContentUpdateResponse created = repo.createContent("this is an awesome file I created\n", "Creating a file for integration tests.", createdFilename);
+        GHContent createdContent = created.getContent();
+
+        assertNotNull(created.getCommit());
+        assertNotNull(created.getContent());
+        assertNotNull(createdContent.getContent());
+        assertEquals("this is an awesome file I created\n", createdContent.getContent());
+
+        GHContentUpdateResponse updatedContentResponse = createdContent.update("this is some new content\n", "Updated file for integration tests.");
+        GHContent updatedContent = updatedContentResponse.getContent();
+
+        assertNotNull(updatedContentResponse.getCommit());
+        assertNotNull(updatedContentResponse.getContent());
+        assertEquals("this is some new content\n", updatedContent.getContent());
+
+        GHContentUpdateResponse deleteResponse = updatedContent.delete("Enough of this foolishness!");
+
+        assertNotNull(deleteResponse.getCommit());
+        assertNull(deleteResponse.getContent());
     }
 }

60fe143b0749815e8aae80bd8d61d563a1ec30c2
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-04-30 13:27:37 -0700

    detect the common problem
    If the caller tries to parse a pull event into a pull_request event, we'd choke with NPE. This change avoids that.

--- src/main/java/org/kohsuke/github/GHEventPayload.java
+++ src/main/java/org/kohsuke/github/GHEventPayload.java
@@ -51,6 +51,8 @@ public abstract class GHEventPayload {
         @Override
         void wrapUp(GitHub root) {
             super.wrapUp(root);
+            if (pull_request==null)
+                throw new IllegalStateException("Expected pull_request payload, but got something else. Maybe we've got another type of event?");
             if (repository!=null) {
                 repository.wrap(root);
                 pull_request.wrap(repository);

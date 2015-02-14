41e0329f55d8cb0138eeb71af7de341a3743a04a
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-02-14 06:52:48 -0800

    doc improvement

--- src/main/java/org/kohsuke/github/Identifiable.java
+++ src/main/java/org/kohsuke/github/Identifiable.java
@@ -3,24 +3,39 @@ package org.kohsuke.github;
 import java.net.URL;
 import java.util.Date;
 
+/**
+ * Most (all?) domain objects in GitHub seems to have these 4 properties.
+ */
 public class Identifiable {
     protected String url;
     protected int id;
     protected String created_at;
     protected String updated_at;
 
+    /**
+     * When was this resource created?
+     */
     public Date getCreatedAt() {
         return GitHub.parseDate(created_at);
     }
 
+    /**
+     * API URL of this object.
+     */
     public URL getUrl() {
         return GitHub.parseURL(url);
     }
 
+    /**
+     * When was this resource last updated?
+     */
     public Date getUpdatedAt() {
         return GitHub.parseDate(updated_at);
     }
 
+    /**
+     * Unique ID number of this resource.
+     */
     public int getId() {
         return id;
     }

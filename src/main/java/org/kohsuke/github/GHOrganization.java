e94c36b7e6f4cb18d3403a05994a03b528584e2e
Daniel Lovera <daniel.lovera@zenika.com>
2015-12-13 21:05:36 +0100

    clean: remove unused import

--- src/main/java/org/kohsuke/github/GHOrganization.java
+++ src/main/java/org/kohsuke/github/GHOrganization.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
 

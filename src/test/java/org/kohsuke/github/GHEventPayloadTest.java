9988a090acd96c345d61a24b947e078cca000760
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-11-11 14:27:09 +0000

    Add some more tests

--- src/test/java/org/kohsuke/github/GHEventPayloadTest.java
+++ src/test/java/org/kohsuke/github/GHEventPayloadTest.java
@@ -202,6 +202,9 @@ public class GHEventPayloadTest {
         assertThat(event.getRef(), is("refs/heads/changes"));
         assertThat(event.getBefore(), is("9049f1265b7d61be4a8904a9a27120d2064dab3b"));
         assertThat(event.getHead(), is("0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c"));
+        assertThat(event.isCreated(), is(false));
+        assertThat(event.isDeleted(), is(false));
+        assertThat(event.isForced(), is(false));
         assertThat(event.getCommits().size(), is(1));
         assertThat(event.getCommits().get(0).getSha(), is("0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c"));
         assertThat(event.getCommits().get(0).getAuthor().getEmail(), is("baxterthehacker@users.noreply.github.com"));

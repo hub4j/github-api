9dabec107ba0e69282d6eb9312d9a0c1bcc7f000
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-09-01 12:52:15 +0100

    Add basic support for tag objects

---
+++ src/main/java/org/kohsuke/github/GHTagObject.java
@@ -0,0 +1,60 @@
+package org.kohsuke.github;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+/**
+ * Represents an annotated tag in a {@link GHRepository}
+ *
+ * @see GHRepository#getAnnotatedTag()
+ */
+@SuppressFBWarnings(value = {"UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD", "UWF_UNWRITTEN_FIELD", 
+    "NP_UNWRITTEN_FIELD"}, justification = "JSON API")
+public class GHTagObject {
+    private GHRepository owner;
+    private GitHub root;
+
+    private String tag;
+    private String sha;
+    private String url;
+    private String message;
+    private GitUser tagger;
+    private GHRef.GHObject object;
+
+    /*package*/ GHTagObject wrap(GHRepository owner) {
+        this.owner = owner;
+        this.root = owner.root;
+        return this;
+    }
+
+    public GHRepository getOwner() {
+        return owner;
+    }
+
+    public GitHub getRoot() {
+        return root;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public String getSha() {
+        return sha;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public GitUser getTagger() {
+        return tagger;
+    }
+
+    public GHRef.GHObject getObject() {
+        return object;
+    }
+}

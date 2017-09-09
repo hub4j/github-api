fe2af19e42c818560e49d8c74282fc8d5f62e076
Kohsuke Kawaguchi <kk@kohsuke.org>
2017-09-09 12:15:21 -0700

    Unused constant

--- src/main/java/org/kohsuke/github/Previews.java
+++ src/main/java/org/kohsuke/github/Previews.java
@@ -8,5 +8,4 @@ package org.kohsuke.github;
     static final String DRAX = "application/vnd.github.drax-preview+json";
     static final String SQUIRREL_GIRL = "application/vnd.github.squirrel-girl-preview";
     static final String KORRA = "application/vnd.github.korra-preview";
-    static final String POLARIS = "application/vnd.github.polaris-preview";
 }

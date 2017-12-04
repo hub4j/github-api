80b9b55cfeb114c5f17e5635b47f14a92dd4d7b5
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-12-04 15:31:20 +0100

    add Travis build file for this repository

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function configureTravis {
+  mkdir -p ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v39 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+. installJDK8
+
+export DEPLOY_PULL_REQUEST=true
+
+regular_mvn_build_deploy_analyze

f0428951e823edb06329e5bdd70479464413a567
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-12-04 15:28:19 +0100

    PullRequestTest can be run with custom org and repo
    passed as command line arguments of the surefire process
    eg.: maven clean package -DargLine="-Dgithub-api.organization=foo -Dgithub-api.pr.repository=bar"

--- src/test/java/org/kohsuke/github/AbstractGitHubApiTestBase.java
+++ src/test/java/org/kohsuke/github/AbstractGitHubApiTestBase.java
@@ -19,23 +19,63 @@
  */
 package org.kohsuke.github;
 
-import java.io.FileInputStream;
-import java.util.Properties;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
 import org.kohsuke.randname.RandomNameGenerator;
 
+import javax.annotation.Nullable;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.*;
+
+import static java.util.Collections.unmodifiableMap;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public abstract class AbstractGitHubApiTestBase extends Assert {
 
+    static final class Property {
+        private final String key;
+        private final String defaultValue;
+
+        Property(String key, String defaultValue) {
+            this.key = checkNotNull(key);
+            this.defaultValue = checkNotNull(defaultValue);
+        }
+
+        private static <T> T checkNotNull(@Nullable T key) {
+            if (key == null) {
+                throw new NullPointerException();
+            }
+            return key;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Property property = (Property) o;
+            return key.equals(property.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return key.hashCode();
+        }
+    }
+
     protected GitHub gitHub;
+
+    private final Set<Property> properties;
+    private Map<Property, String> propertyValues = Collections.emptyMap();
+
+    public AbstractGitHubApiTestBase(Property... properties) {
+        this.properties = Collections.unmodifiableSet(new HashSet<Property>(Arrays.asList(properties)));
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -51,10 +91,32 @@ public abstract class AbstractGitHubApiTestBase extends Assert {
             }
             // use the non-standard credential preferentially, so that developers of this library do not have
             // to clutter their event stream.
-            gitHub = GitHubBuilder.fromProperties(props).withRateLimitHandler(RateLimitHandler.FAIL).build();
+            this.gitHub = GitHubBuilder.fromProperties(props).withRateLimitHandler(RateLimitHandler.FAIL).build();
         } else {
-            gitHub = GitHubBuilder.fromCredentials().withRateLimitHandler(RateLimitHandler.FAIL).build();
+            this.gitHub = GitHubBuilder.fromCredentials().withRateLimitHandler(RateLimitHandler.FAIL).build();
         }
+        this.propertyValues = unmodifiableMap(readPropertyValues());
+    }
+
+    private Map<Property, String> readPropertyValues() {
+        Map<Property, String> res = new HashMap<>();
+        Map<String, String> env = System.getenv();
+        for (Property property : properties) {
+            String value = env.get(property.key);
+            res.put(property, value == null ? property.defaultValue : value);
+        }
+        return res;
+    }
+
+    /**
+     * @throws IllegalArgumentException if {@code property} hasn't been registered at startup
+     */
+    String getValue(Property property) {
+        String res = propertyValues.get(property);
+        if (res == null) {
+            return property.defaultValue;
+        }
+        return res;
     }
 
     protected GHUser getUser() {

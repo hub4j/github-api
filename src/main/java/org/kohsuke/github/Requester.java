892d30516546be46f7cebd3a0f920db0801b3e29
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-01-08 15:29:22 +0100

    add Requester#with override for long value

--- src/main/java/org/kohsuke/github/Requester.java
+++ src/main/java/org/kohsuke/github/Requester.java
@@ -134,6 +134,10 @@ class Requester {
         return _with(key, value);
     }
 
+    public Requester with(String key, long value) {
+        return _with(key, value);
+    }
+
     public Requester with(String key, Integer value) {
         if (value!=null)
             _with(key, value);

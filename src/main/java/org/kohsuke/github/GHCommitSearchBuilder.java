hub4j/github-api

commit 8b38a20c1849606103dd4f676d5c27613df0b7d8

Update commons-lang to 3.7
This fixes the vulnerabilities:
* https://issues.apache.org/jira/browse/LANG-1373
* https://issues.apache.org/jira/browse/LANG-805.

pom.xml
@@ -96,9 +96,9 @@
 
   <dependencies>
     <dependency>
-      <groupId>commons-lang</groupId>
-      <artifactId>commons-lang</artifactId>
-      <version>2.6</version>
+      <groupId>org.apache.commons</groupId>
+      <artifactId>commons-lang3</artifactId>
+      <version>3.7</version>
     </dependency>
     <dependency>
       <groupId>commons-codec</groupId>

commit 86f868b2d420eaf7d4d40123f219c520cd4e56ca

Fixed compilation errors introduced by 8b38a20c1849606103dd4f676d5c27613df0b7d8

pom.xml
@@ -7,7 +7,7 @@
   </parent>
 
   <artifactId>github-api</artifactId>
-  <version>1.93-SNAPSHOT</version>
+  <version>1.93</version>
   <name>GitHub API for Java</name>
   <url>http://github-api.kohsuke.org/</url>
   <description>GitHub API for Java</description>
@@ -16,7 +16,7 @@
     <connection>scm:git:git@github.com/kohsuke/${project.artifactId}.git</connection>
     <developerConnection>scm:git:ssh://git@github.com/kohsuke/${project.artifactId}.git</developerConnection>
     <url>http://${project.artifactId}.kohsuke.org/</url>
-    <tag>HEAD</tag>
+    <tag>github-api-1.93</tag>
   </scm>
 
   <distributionManagement>

src/main/java/org/kohsuke/github/GHCommitSearchBuilder.java
@@ -1,6 +1,6 @@
 package org.kohsuke.github;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 

src/main/java/org/kohsuke/github/GHDeployKey.java
@@ -1,6 +1,6 @@
 package org.kohsuke.github;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.IOException;
 

src/main/java/org/kohsuke/github/GHKey.java
@@ -1,7 +1,7 @@
 package org.kohsuke.github;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * SSH public key.

src/main/java/org/kohsuke/github/GHObject.java
@@ -2,8 +2,8 @@ package org.kohsuke.github;
 
 import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import javax.annotation.CheckForNull;
 import java.io.IOException;

src/main/java/org/kohsuke/github/GHPullRequest.java
@@ -347,9 +347,9 @@ public class GHPullRequest extends GHIssue {
     }
 
     public void requestReviewers(List<GHUser> reviewers) {
-        return new Requester(root).method("POST")
+        new Requester(root).method("POST")
                 .with("reviewers", toLogins(reviewers))
-                .to(getApiRoute() + REQUEST_REVIEWERS)
+                .to(getApiRoute() + REQUEST_REVIEWERS);
     }
     
     /**

src/main/java/org/kohsuke/github/GHRepository.java
@@ -27,7 +27,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;

src/main/java/org/kohsuke/github/GHSearchBuilder.java
@@ -1,6 +1,6 @@
 package org.kohsuke.github;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;

src/main/java/org/kohsuke/github/Requester.java
@@ -26,7 +26,7 @@ package org.kohsuke.github;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.WillClose;
@@ -63,7 +63,7 @@ import static java.util.Arrays.asList;
 import static java.util.logging.Level.FINE;
 import static java.util.logging.Level.FINEST;
 import static java.util.logging.Level.INFO;
-import static org.apache.commons.lang.StringUtils.defaultString;
+import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.kohsuke.github.GitHub.MAPPER;
 
 /**
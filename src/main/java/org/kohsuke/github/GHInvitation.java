fe5ea52cdf9d2af3b434ac28428fab78270d7bdd
Rechi <Rechi@users.noreply.github.com>
2018-05-29 22:00:00 +0200

    [feature] implement Repository Invitations API
    fixes #374

---
+++ src/main/java/org/kohsuke/github/GHInvitation.java
@@ -0,0 +1,45 @@
+package org.kohsuke.github;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class GHInvitation extends GHObject {
+    /*package almost final*/ GitHub root;
+
+    private int id;
+    private GHRepository repository;
+    private GHUser invitee, inviter;
+    private String permissions;
+    private String html_url;
+
+    /*package*/ GHInvitation wrapUp(GitHub root) {
+        this.root = root;
+        return this;
+    }
+
+    /**
+     * Accept a repository invitation.
+     */
+    public void accept() throws IOException {
+        root.retrieve().method("PATCH").to("/user/repository_invitations/" + id);
+    }
+
+    /**
+     * Decline a repository invitation.
+     */
+    public void decline() throws IOException {
+        root.retrieve().method("DELETE").to("/user/repository_invitations/" + id);
+    }
+
+    @Override
+    public URL getHtmlUrl() {
+        return GitHub.parseURL(html_url);
+    }
+}

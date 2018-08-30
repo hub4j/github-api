92a015ca4df1ccdf0d0b7b049e951bfe571fb09c
Kohsuke Kawaguchi <kk@kohsuke.org>
2018-08-29 20:18:10 -0700

    Clear up import statements

--- src/main/java/org/kohsuke/github/GHInvitation.java
+++ src/main/java/org/kohsuke/github/GHInvitation.java
@@ -2,13 +2,6 @@ package org.kohsuke.github;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * @see GitHub#getMyInvitations()

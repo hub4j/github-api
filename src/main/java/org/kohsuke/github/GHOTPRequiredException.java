e325bf77715531775f6de3f4f75cf7f111f0ac44
Kevin Harrington <mad.hephaestus@gmail.com>
2019-11-13 00:38:51 -0500

    fixed for the sensitive javadoc parser

--- src/main/java/org/kohsuke/github/GHOTPRequiredException.java
+++ src/main/java/org/kohsuke/github/GHOTPRequiredException.java
@@ -1,7 +1,8 @@
 package org.kohsuke.github;
 /**
  * This exception is thrown when GitHub is requesting an OTP from the user
- * @author Kevin Harrington <mad.hephaestus@gmail.com>
+ * 
+ * @author Kevin Harrington mad.hephaestus@gmail.com
  *
  */
 public class GHOTPRequiredException extends GHIOException {
--- src/test/java/org/kohsuke/github/Github2faTest.java
+++ src/test/java/org/kohsuke/github/Github2faTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.junit.Test;
 /**
- * @author Kevin Harrington <mad.hephaestus@gmail.com>
+ * @author Kevin Harrington mad.hephaestus@gmail.com
  */
 public class Github2faTest  extends AbstractGitHubWireMockTest {
 

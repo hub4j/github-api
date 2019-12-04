105763666693e5113909481d25d75eea22b1e008
Liam Newman <bitwiseman@gmail.com>
2019-12-04 16:25:33 +0000

    Add wiremock data

--- src/test/java/org/kohsuke/github/GHObjectTest.java
+++ src/test/java/org/kohsuke/github/GHObjectTest.java
@@ -2,13 +2,15 @@ package org.kohsuke.github;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 public class GHObjectTest extends org.kohsuke.github.AbstractGitHubWireMockTest {
 
     @Test
     public void test_toString() throws Exception {
         GHOrganization org = gitHub.getOrganization(GITHUB_API_TEST_ORG);
-
+        assertThat(org.toString(),
+                containsString(
+                        "login=github-api-test-org,location=<null>,blog=<null>,email=<null>,name=<null>,company=<null>,followers=0,following=0"));
     }
 }

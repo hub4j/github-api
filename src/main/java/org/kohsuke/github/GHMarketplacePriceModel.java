1d983a009059bcf3f91cd0712ce3f30768d0c5e5
PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>
2019-12-08 20:13:54 +1300

    Implement list marketplace plans
    Signed-off-by: PauloMigAlmeida <paulo.miguel.almeida.rodenas@gmail.com>

---
+++ src/main/java/org/kohsuke/github/GHMarketplacePriceModel.java
@@ -0,0 +1,21 @@
+package org.kohsuke.github;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum GHMarketplacePriceModel {
+    FREE("free"), PER_UNIT("per-unit"), FLAT_RATE("flat-rate");
+
+    @JsonValue
+    private final String internalName;
+
+    GHMarketplacePriceModel(String internalName) {
+        this.internalName = internalName;
+    }
+
+    /**
+     * Returns GitHub's internal representation of this event.
+     */
+    public String symbol(){
+        return internalName;
+    }
+}

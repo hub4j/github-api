98b606287d5976dbcff2f31c0215f7950a700bc6
Liam Newman <bitwiseman@gmail.com>
2020-01-18 23:33:44 -0800

    Add more validation to rate limit
    Downstream tests are doing some strange Json.  Give better output when they do something invalid.

--- src/main/java/org/kohsuke/github/JsonRateLimit.java
+++ src/main/java/org/kohsuke/github/JsonRateLimit.java
@@ -1,10 +1,22 @@
 package org.kohsuke.github;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
 /**
  * @author Kohsuke Kawaguchi
  */
 class JsonRateLimit {
 
-    GHRateLimit resources;
+    @Nonnull
+    final GHRateLimit resources;
 
+    @JsonCreator
+    private JsonRateLimit(@Nonnull @JsonProperty(value = "resources", required = true) GHRateLimit resources) {
+        Objects.requireNonNull(resources);
+        this.resources = resources;
+    }
 }

df576e273897d12554dfa759451f6ff16e3627a5
Mårten Gustafson <marten.gustafson@gmail.com>
2020-04-14 11:25:10 +0200

    Expose and test per issue event attributes for milestone, label and assignment events.

--- src/main/java/org/kohsuke/github/GHIssueEvent.java
+++ src/main/java/org/kohsuke/github/GHIssueEvent.java
@@ -18,6 +18,9 @@ public class GHIssueEvent {
     private String commit_id;
     private String commit_url;
     private String created_at;
+    private GHMilestone milestone;
+    private GHLabel label;
+    private GHUser assignee;
 
     private GHIssue issue;
 
@@ -109,6 +112,36 @@ public class GHIssueEvent {
      */
     public GHIssue getIssue() {
         return issue;
+    }
+
+    /**
+     * Get the {@link GHMilestone} that this issue was added to or removed from. Only present for events "milestoned"
+     * and "demilestoned", <code>null</code> otherwise.
+     *
+     * @return the milestone
+     */
+    public GHMilestone getMilestone() {
+        return milestone;
+    }
+
+    /**
+     * Get the {@link GHLabel} that was added to or removed from the issue. Only present for events "labeled" and
+     * "unlabeled", <code>null</code> otherwise.
+     *
+     * @return the label
+     */
+    public GHLabel getLabel() {
+        return label;
+    }
+
+    /**
+     * Get the {@link GHUser} that was assigned or unassigned from the issue. Only present for events "assigned" and
+     * "unassigned", <code>null</code> otherwise.
+     *
+     * @return the user
+     */
+    public GHUser getAssignee() {
+        return assignee;
     }
 
     GHIssueEvent wrapUp(GitHub root) {

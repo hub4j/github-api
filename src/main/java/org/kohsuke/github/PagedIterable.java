ac97147c1f4d6e70c5152891da313a9f52cc3418
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2020-05-16 09:58:20 +0200

    Remove unused imports

--- src/main/java/org/kohsuke/github/PagedIterable.java
+++ src/main/java/org/kohsuke/github/PagedIterable.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;

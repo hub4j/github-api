965c9cb0af975d06e32a8c78fa9edbaa49a14302
Liam Newman <bitwiseman@gmail.com>
2020-05-20 17:03:35 -0700

    Fixed getRef() to use git/ref endpoint instead of git/refs
    Fixes #794

--- src/main/java/org/kohsuke/github/GHRepository.java
+++ src/main/java/org/kohsuke/github/GHRepository.java
@@ -1567,6 +1567,9 @@ public class GHRepository extends GHObject {
      *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
      */
     public PagedIterable<GHRef> listRefs(String refType) throws IOException {
+        if (refType.startsWith("refs/")) {
+            refType = refType.replaceFirst("refs/", "");
+        }
         final String url = String.format("/repos/%s/%s/git/refs/%s", getOwnerName(), name, refType);
         return root.createRequest().withUrlPath(url).toIterable(GHRef[].class, item -> item.wrap(root));
     }
@@ -1587,7 +1590,7 @@ public class GHRepository extends GHObject {
         }
 
         return root.createRequest()
-                .withUrlPath(getApiTailUrl(String.format("git/refs/%s", refName)))
+                .withUrlPath(getApiTailUrl(String.format("git/ref/%s", refName)))
                 .fetch(GHRef.class)
                 .wrap(root);
     }

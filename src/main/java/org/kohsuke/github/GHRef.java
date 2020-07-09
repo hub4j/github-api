580f2537f2b6dfa73792cd488cbf7f08b532b63b
Liam Newman <bitwiseman@gmail.com>
2020-07-09 15:00:18 -0700

    Move GHRef readers to GHRef class

--- src/main/java/org/kohsuke/github/GHRef.java
+++ src/main/java/org/kohsuke/github/GHRef.java
@@ -1,5 +1,6 @@
 package org.kohsuke.github;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.io.IOException;
@@ -88,6 +89,78 @@ public class GHRef {
     GHRef wrap(GitHub root) {
         this.root = root;
         return this;
+    }
+
+    /**
+     * Retrive a ref of the given type for the current GitHub repository.
+     *
+     * @param repository
+     *            the repository to read from
+     * @param refName
+     *            eg: heads/branch
+     * @return refs matching the request type
+     * @throws IOException
+     *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
+     */
+    static GHRef read(GHRepository repository, String refName) throws IOException {
+        // Also accept e.g. "refs/heads/branch" for consistency with createRef().
+        if (refName.startsWith("refs/")) {
+            refName = refName.replaceFirst("refs/", "");
+        }
+
+        // We would expect this to use `git/ref/%s` but some versions of GHE seem to not support it
+        // Instead use `git/refs/%s` and check the result actually matches the ref
+        GHRef result = null;
+        try {
+            result = repository.root.createRequest()
+                    .withUrlPath(repository.getApiTailUrl(String.format("git/refs/%s", refName)))
+                    .fetch(GHRef.class)
+                    .wrap(repository.root);
+        } catch (IOException e) {
+            // If the parse exception is due to the above returning an array instead of a single ref
+            // that means the individual ref did not exist. Handled by result check below.
+            // Otherwise, rethrow.
+            if (!(e.getCause() instanceof JsonMappingException)) {
+                throw e;
+            }
+        }
+
+        // Verify that the ref returned is the one requested
+        // Used .endsWith(refName) instead of .equals("refs/" + refName) to workaround a GitBucket
+        // issue where the "ref" field omits the "refs/" prefix. "endsWith()" is functionally
+        // the same for this scenario - the server refs matching is prefix-based, so
+        // a ref that ends with the correct string will always be the correct one.
+        if (result == null || !result.getRef().endsWith(refName)) {
+            throw new GHFileNotFoundException(String.format("git/refs/%s", refName)
+                    + " {\"message\":\"Not Found\",\"documentation_url\":\"https://developer.github.com/v3/git/refs/#get-a-reference\"}");
+        }
+        return result;
+    }
+
+    /**
+     * Retrieves all refs of the given type for the current GitHub repository.
+     *
+     * @param repository
+     *            the repository to read from
+     * @param refType
+     *            the type of reg to search for e.g. <code>tags</code> or <code>commits</code>
+     * @return paged iterable of all refs of the specified type
+     * @throws IOException
+     *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
+     */
+    static PagedIterable<GHRef> readMatching(GHRepository repository, String refType) throws IOException {
+        if (refType.startsWith("refs/")) {
+            refType = refType.replaceFirst("refs/", "");
+        }
+
+        String url = repository.getApiTailUrl(String.format("git/refs/%s", refType));
+        // if no types, do not end with slash just to be safe.
+        if (refType.equals("")) {
+            url = url.substring(0, url.length() - 1);
+        }
+        return repository.root.createRequest()
+                .withUrlPath(url)
+                .toIterable(GHRef[].class, item -> item.wrap(repository.root));
     }
 
     /**
--- src/main/java/org/kohsuke/github/GHRepository.java
+++ src/main/java/org/kohsuke/github/GHRepository.java
@@ -25,7 +25,6 @@ package org.kohsuke.github;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -1541,8 +1540,7 @@ public class GHRepository extends GHObject {
      *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
      */
     public PagedIterable<GHRef> listRefs() throws IOException {
-        final String url = String.format("/repos/%s/%s/git/refs", getOwnerName(), name);
-        return root.createRequest().withUrlPath(url).toIterable(GHRef[].class, item -> item.wrap(root));
+        return listRefs("");
     }
 
     /**
@@ -1568,11 +1566,7 @@ public class GHRepository extends GHObject {
      *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
      */
     public PagedIterable<GHRef> listRefs(String refType) throws IOException {
-        if (refType.startsWith("refs/")) {
-            refType = refType.replaceFirst("refs/", "");
-        }
-        final String url = String.format("/repos/%s/%s/git/refs/%s", getOwnerName(), name, refType);
-        return root.createRequest().withUrlPath(url).toIterable(GHRef[].class, item -> item.wrap(root));
+        return GHRef.readMatching(this, refType);
     }
 
     /**
@@ -1585,38 +1579,7 @@ public class GHRepository extends GHObject {
      *             on failure communicating with GitHub, potentially due to an invalid ref type being requested
      */
     public GHRef getRef(String refName) throws IOException {
-        // Also accept e.g. "refs/heads/branch" for consistency with createRef().
-        if (refName.startsWith("refs/")) {
-            refName = refName.replaceFirst("refs/", "");
-        }
-
-        // We would expect this to use `git/ref/%s` but some versions of GHE seem to not support it
-        // Instead use `git/refs/%s` and check the result actually matches the ref
-        GHRef result = null;
-        try {
-            result = root.createRequest()
-                    .withUrlPath(getApiTailUrl(String.format("git/refs/%s", refName)))
-                    .fetch(GHRef.class)
-                    .wrap(root);
-        } catch (IOException e) {
-            // If the parse exception is due to the above returning an array instead of a single ref
-            // that means the individual ref did not exist
-            if (!(e.getCause() instanceof JsonMappingException)) {
-                throw e;
-            }
-        }
-
-        // Verify that the ref returned is the one requested
-        // Used .endsWith(refName) instead of .equals("refs/" + refName) to workaround a GitBucket
-        // issue where the "ref" field omits the "refs/" prefix. "endsWith()" is functionally
-        // the same for this scenario - the server refs matching is prefix-based, so
-        // a ref that ends with the correct string will always be the correct one.
-        if (result == null || !result.getRef().endsWith(refName)) {
-            throw new GHFileNotFoundException(String.format("git/refs/%s", refName)
-                    + " {\"message\":\"Not Found\",\"documentation_url\":\"https://developer.github.com/v3/git/refs/#get-a-reference\"}");
-        }
-        return result;
-
+        return GHRef.read(this, refName);
     }
 
     /**

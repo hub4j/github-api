72a1c24b3b48f70301d3a85d733b294bf6ff378d
Joe <jkalash@expedia.com>
2020-07-30 21:43:37 +0100

    format

--- src/main/java/org/kohsuke/github/GHCheckSuite.java
+++ src/main/java/org/kohsuke/github/GHCheckSuite.java
@@ -7,9 +7,7 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Represents a check suite.

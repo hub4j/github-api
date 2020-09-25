3f021f9552536be077939f201b6302a50970df12
Marcos.Cela <marcosclop@ext.inditex.com>
2020-09-25 10:56:07 +0200

    lint CredentialProvider

--- src/main/java/org/kohsuke/github/CredentialProvider.java
+++ src/main/java/org/kohsuke/github/CredentialProvider.java
@@ -1,20 +1,22 @@
 package org.kohsuke.github;
 
 /**
- * Provides a functional interface that returns a valid encodedAuthorization. This strategy allows
- * for a provider that dynamically changes the credentials. Each request will request the credentials
- * from the provider.
+ * Provides a functional interface that returns a valid encodedAuthorization. This strategy allows for a provider that
+ * dynamically changes the credentials. Each request will request the credentials from the provider.
  */
 public interface CredentialProvider {
     /**
-     * Returns the credentials to be used with a given request. As an example, a credential
-     * provider for a bearer token will return something like:
-     * <pre>{@code
-     *  @Override
+     * Returns the credentials to be used with a given request. As an example, a credential provider for a bearer token
+     * will return something like:
+     * 
+     * <pre>
+     * {@code
+     *  &#64;Override
      *  public String getEncodedAuthorization() {
      *  return "Bearer myBearerToken";
      *  }
-     * }</pre>
+     * }
+     * </pre>
      *
      * @return encoded authorization string, can be null
      */

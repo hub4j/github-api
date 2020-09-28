551be49a1ae6f728424172b1cdc1c23d7782136d
Marcos.Cela <marcosclop@ext.inditex.com>
2020-09-28 10:43:43 +0200

    utility methods on ImmutableCredentialProvider
    These methods let us build the most-used cases
    for static credentials that will never change:
    - JWT credentials
    - Token-based credentials
    - Basic Auth credentials

--- src/main/java/org/kohsuke/github/ImmutableCredentialProvider.java
+++ src/main/java/org/kohsuke/github/ImmutableCredentialProvider.java
@@ -1,5 +1,9 @@
 package org.kohsuke.github;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 /**
  * A {@link CredentialProvider} that always returns the same credentials
  */
@@ -9,6 +13,53 @@ public class ImmutableCredentialProvider implements CredentialProvider {
 
     public ImmutableCredentialProvider(String authorization) {
         this.authorization = authorization;
+    }
+
+    /**
+     * Builds and returns a {@link CredentialProvider} from a given oauthAccessToken
+     *
+     * @param oauthAccessToken
+     * @return a correctly configured {@link CredentialProvider} that will always return the same provided
+     *         oauthAccessToken
+     */
+    public static CredentialProvider fromOauthToken(String oauthAccessToken) {
+        return new ImmutableCredentialProvider(String.format("token %s", oauthAccessToken));
+    }
+
+    /**
+     * Builds and returns a {@link CredentialProvider} from a given jwtToken
+     *
+     * @param jwtToken
+     *            The JWT token
+     * @return a correctly configured {@link CredentialProvider} that will always return the same provided jwtToken
+     * @see <a href="https://jwt.io/introduction/">jwt.io/introduction</a>
+     * @see <a href=
+     *      "https://docs.github.com/en/free-pro-team@latest/developers/apps/authenticating-with-github-apps#authenticating-as-a-github-app">Authenticating
+     *      as a GitHub App</a>
+     */
+    public static CredentialProvider fromJwtToken(String jwtToken) {
+        return new ImmutableCredentialProvider(String.format("Bearer %s", jwtToken));
+    }
+
+    /**
+     * Builds and returns a {@link CredentialProvider} from the given user/password pair
+     *
+     * @param login
+     *            The login for the user, usually the same as the username
+     * @param password
+     *            The password for the associated user
+     * @return a correctly configured {@link CredentialProvider} that will always return the credentials for the same
+     *         user and password combo
+     * @throws UnsupportedEncodingException
+     *             the character encoding is not supported
+     */
+    public static CredentialProvider fromLoginAndPassword(String login, String password)
+            throws UnsupportedEncodingException {
+        String authorization = (String.format("%s:%s", login, password));
+        String charsetName = StandardCharsets.UTF_8.name();
+        String b64encoded = Base64.getEncoder().encodeToString(authorization.getBytes(charsetName));
+        String encodedAuthorization = String.format("Basic %s", b64encoded);
+        return new ImmutableCredentialProvider(encodedAuthorization);
     }
 
     @Override

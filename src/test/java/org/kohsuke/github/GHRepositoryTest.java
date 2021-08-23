1c1799ef7af87e32e615a428002544712519f8b0
Liam Newman <bitwiseman@gmail.com>
2021-08-23 13:58:58 -0700

    Update src/test/java/org/kohsuke/github/GHRepositoryTest.java

--- src/test/java/org/kohsuke/github/GHRepositoryTest.java
+++ src/test/java/org/kohsuke/github/GHRepositoryTest.java
@@ -872,7 +872,7 @@ public class GHRepositoryTest extends AbstractGitHubWireMockTest {
         GHCompare compare = getRepository().getCompare("e46a9f3f2ac55db96de3c5c4706f2813b3a96465",
                 "8051615eff597f4e49f4f47625e6fc2b49f26bfc",
                 10,
-                1);        
+                1);
         assertThat(compare.getTotalCommits(), is(9));
 
     }

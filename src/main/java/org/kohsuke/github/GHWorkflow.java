386d7293a5c527f6f2b7229c8944077751939ff5
yixingz3 <88013471+yixingz3@users.noreply.github.com>
2021-12-07 17:31:42 -0500

    Update src/main/java/org/kohsuke/github/GHWorkflow.java
    Co-authored-by: Liam Newman <bitwiseman@gmail.com>

--- src/main/java/org/kohsuke/github/GHWorkflow.java
+++ src/main/java/org/kohsuke/github/GHWorkflow.java
@@ -122,8 +122,6 @@ public class GHWorkflow extends GHObject {
      *            default properties configured in the workflow file will be used when inputs are omitted.
      * @throws IOException
      *             the io exception
-     * @throws IllegalArgumentException
-     *             the illegal argument exception
      */
     public void dispatch(String ref, Map<String, Object> inputs) throws IOException {
         if (ref == null || ref.length() == 0) {

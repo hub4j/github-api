9591e4d9e606c28af403f07e4bf63c28bcf62124
Liam Newman <bitwiseman@gmail.com>
2023-01-25 10:32:05 -0800

    Update WireMockRule.java

--- src/test/java/org/kohsuke/github/junit/WireMockRule.java
+++ src/test/java/org/kohsuke/github/junit/WireMockRule.java
@@ -528,6 +528,16 @@ public class WireMockRule implements MethodRule, TestRule, Container, Stubbing, 
     }
 
     /**
+     * Removes the stub mapping.
+     *
+     * @param id
+     *            the id
+     */
+    public void removeStubMapping(UUID id) {
+        wireMockServer.removeStubMapping(id);
+    }
+
+    /**
      * List all stub mappings.
      *
      * @return the list stub mappings result

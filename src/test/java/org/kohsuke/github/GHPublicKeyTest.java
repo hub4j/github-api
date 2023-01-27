4c01091950ebbcbf06b2a7a787e9d6f10a37c146
Jonas van Vliet <jvv@trackunit.com>
2023-01-27 09:29:08 +0100

    Refreshed wiremocks to the GHPublicKeyTests class and ensured that the test uses them.

--- src/test/java/org/kohsuke/github/GHPublicKeyTest.java
+++ src/test/java/org/kohsuke/github/GHPublicKeyTest.java
@@ -3,13 +3,9 @@ package org.kohsuke.github;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.KeyPair;
-import org.junit.After;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * The Class GHPublicKeyTest.
@@ -18,6 +14,7 @@ import java.util.stream.Collectors;
  */
 public class GHPublicKeyTest extends AbstractGitHubWireMockTest {
     public static final String TMP_KEY_NAME = "Temporary user key";
+    public static final String WIREMOCK_SSH_PUBLIC_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDepW2/BSVFM2AfuGGsvi+vjQzC0EBD3R+/7PNEvP0/nvTWxiC/tthfvvCJR6TKrsprCir5tiJFm73gX+K18W0RKYpkyg8H6d1eZu3q/JOiGvoDPeN8Oe9hOGeeexw1WOiz7ESPHzZYXI981evzHAzxxn8zibr2EryopVNsXyoenw==";
 
     /**
      * Test adding a public key to the user
@@ -27,23 +24,25 @@ public class GHPublicKeyTest extends AbstractGitHubWireMockTest {
      */
     @Test
     public void testAddPublicKey() throws Exception {
-        GHMyself me = gitHub.getMyself();
+        GHKey newPublicKey = null;
         try {
-            me.addPublicKey(TMP_KEY_NAME, createPublicKey());
-        } finally {
-            cleanupTemporaryKeys();
-        }
-    }
+            GHMyself me = gitHub.getMyself();
 
-    @After
-    public void cleanupTemporaryKeys() throws IOException {
-        GHMyself me = gitHub.getMyself();
-        List<GHKey> tmpGhKeys = me.getPublicKeys()
-                .stream()
-                .filter(key -> key.getTitle().equals(TMP_KEY_NAME))
-                .collect(Collectors.toList());
-        for (GHKey ghKey : tmpGhKeys) {
-            ghKey.delete();
+            // This test adds a public key to the users profile. Since such a key grants access to everything the user
+            // can access, the private key must be a secret that only the current user can access.
+            //
+            // The following if-statement ensures that the test does not add keys when using wiremocks, but creates
+            // a unique key when run without wiremocks.
+            //
+            if (this.mockGitHub.isUseProxy()) {
+                newPublicKey = me.addPublicKey(TMP_KEY_NAME, WIREMOCK_SSH_PUBLIC_KEY);
+            } else {
+                newPublicKey = me.addPublicKey(TMP_KEY_NAME, createPublicKey());
+            }
+        } finally {
+            if (newPublicKey != null) {
+                newPublicKey.delete();
+            }
         }
     }
 

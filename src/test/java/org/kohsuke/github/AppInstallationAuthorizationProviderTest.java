6cf735603a20b71c3ca05681e091b62dcbfd7527
Pierre Beitz <pibeitz@gmail.com>
2023-04-14 11:19:03 +0200

    add test

--- src/test/java/org/kohsuke/github/AppInstallationAuthorizationProviderTest.java
+++ src/test/java/org/kohsuke/github/AppInstallationAuthorizationProviderTest.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 // TODO: Auto-generated Javadoc
+
 /**
  * The Class AppInstallationAuthorizationProviderTest.
  */
@@ -59,6 +61,29 @@ public class AppInstallationAuthorizationProviderTest extends AbstractGHAppInsta
 
         assertThat(encodedAuthorization, notNullValue());
         assertThat(encodedAuthorization, equalTo("token v1.9a12d913f980a45a16ac9c3a9d34d9b7sa314cb6"));
+    }
+
+    /**
+     * Lookup of an app by id works as expected
+     *
+     * @throws IOException
+     *             Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void validJWTTokenWhenLookingUpAppById() throws IOException {
+        AppInstallationAuthorizationProvider provider = new AppInstallationAuthorizationProvider(
+                // https://github.com/organizations/hub4j-test-org/settings/installations/12129901
+                app -> app.getInstallationById(12129901L),
+                jwtProvider1);
+        gitHub = getGitHubBuilder().withAuthorizationProvider(provider)
+                .withEndpoint(mockGitHub.apiServer().baseUrl())
+                .build();
+        String encodedAuthorization = provider.getEncodedAuthorization();
+
+        assertThat(encodedAuthorization, notNullValue());
+        // we could assert on the exact token with wiremock, but it would make the update of the test more complex
+        // do we really care, getting a token should be enough.
+        assertThat(encodedAuthorization, startsWith("token ghs_"));
     }
 
 }

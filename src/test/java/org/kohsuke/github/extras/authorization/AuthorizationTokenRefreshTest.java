e8be18d577c1e7d705797be41dfdd27cc3ca20c9
R. Emre Basar <emre.basar@meltwater.com>
2023-08-10 11:47:42 +0200

    Add tests for re-preparing request on refreshed tokens

---
+++ src/test/java/org/kohsuke/github/extras/authorization/AuthorizationTokenRefreshTest.java
@@ -0,0 +1,54 @@
+package org.kohsuke.github.extras.authorization;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.Test;
+import org.kohsuke.github.AbstractGitHubWireMockTest;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.RateLimitHandler;
+import org.kohsuke.github.authorization.AuthorizationProvider;
+
+import java.io.IOException;
+
+public class AuthorizationTokenRefreshTest extends AbstractGitHubWireMockTest {
+
+    public AuthorizationTokenRefreshTest() throws IOException {
+        useDefaultGitHub = false;
+    }
+
+    @Override
+    protected WireMockConfiguration getWireMockOptions() {
+        return super.getWireMockOptions().extensions(templating.newResponseTransformer());
+    }
+
+    @Test
+    public void testRetriedRequestGetsNewAuthorizationTokenWhenOldOneExpires() throws IOException {
+        snapshotNotAllowed();
+        gitHub = getGitHubBuilder().withAuthorizationProvider(new RefreshingAuthorizationProvider())
+                .withEndpoint(mockGitHub.apiServer().baseUrl())
+                .withRateLimitHandler(RateLimitHandler.WAIT).build();
+        final GHUser kohsuke = gitHub.getUser("kohsuke");
+        assertThat("Usernames match", "kohsuke".equals(kohsuke.getLogin()));
+    }
+
+    @Test
+    public void testRetriedRequestDoesNotGetNewAuthorizationTokenWhenOldOneIsStillValid() throws IOException {
+        gitHub = getGitHubBuilder().withAuthorizationProvider(() -> "original token")
+                .withEndpoint(mockGitHub.apiServer().baseUrl())
+                .withRateLimitHandler(RateLimitHandler.WAIT).build();
+        final GHUser kohsuke = gitHub.getUser("kohsuke");
+        assertThat("Usernames match", "kohsuke".equals(kohsuke.getLogin()));
+    }
+
+    static class RefreshingAuthorizationProvider implements AuthorizationProvider {
+        private boolean used = false;
+
+        @Override
+        public String getEncodedAuthorization() {
+            if (used) {
+                return "refreshed token";
+            }
+            used = true;
+            return "original token";
+        }
+    }
+}

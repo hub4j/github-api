8c51f6b227cf338d9955ccc6d022a8f5ccb2b991
Liam Newman <bitwiseman@gmail.com>
2023-11-08 02:26:20 -0800

    Remove JwtReflectiveBuilderException

--- src/main/java/org/kohsuke/github/extras/authorization/JwtReflectiveBuilderException.java
+++
@@ -1,7 +0,0 @@
-package org.kohsuke.github.extras.authorization;
-
-public class JwtReflectiveBuilderException extends RuntimeException {
-    JwtReflectiveBuilderException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}

8032876a6861ac8f836c2fad389b1a75521f1cc2
D061587 <benjamin.ihrig@sap.com>
2023-12-14 15:06:46 +0100

    Add links

--- src/main/java/org/kohsuke/github/GHRepository.java
+++ src/main/java/org/kohsuke/github/GHRepository.java
@@ -3648,6 +3648,7 @@ public class GHRepository extends GHObject {
      *
      * @return list of top referral paths
      * @throws IOException the io exception
+     * @link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-paths
      */
     public List<GHRepositoryTrafficTopReferralPath> getTopReferralPaths() throws IOException {
         return root().createRequest().method("GET")
@@ -3660,6 +3661,7 @@ public class GHRepository extends GHObject {
      *
      * @return list of top referrers
      * @throws IOException the io exception
+     * @link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-sources
      */
     public List<GHRepositoryTrafficTopReferralSources> getTopReferralSources() throws IOException {
         return root().createRequest().method("GET")

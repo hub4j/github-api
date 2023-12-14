dced813ffc9bc409380c4afd37beb6b85df1f4ce
D061587 <benjamin.ihrig@sap.com>
2023-12-14 15:02:01 +0100

    Add methods to get the referral and referrer metrics

---
+++ src/main/java/org/kohsuke/github/GHRepositoryTrafficReferralBase.java
@@ -0,0 +1,45 @@
+package org.kohsuke.github;
+
+/**
+ * Base class for traffic referral objects.
+ */
+public class GHRepositoryTrafficReferralBase {
+    private int count;
+    private int uniques;
+
+    /**
+     * Instantiates a new Gh repository traffic referral base.
+     */
+    GHRepositoryTrafficReferralBase() {}
+
+    /**
+     * Instantiates a new Gh repository traffic referral base.
+     *
+     * @param count
+     *            the count
+     * @param uniques
+     *            the uniques
+     */
+    GHRepositoryTrafficReferralBase(int count, int uniques) {
+        this.count = count;
+        this.uniques = uniques;
+    }
+
+    /**
+     * Gets count.
+     *
+     * @return the count
+     */
+    public int getCount() {
+        return this.count;
+    }
+
+    /**
+     * Gets uniques.
+     *
+     * @return the uniques
+     */
+    public int getUniques() {
+        return this.uniques;
+    }
+}

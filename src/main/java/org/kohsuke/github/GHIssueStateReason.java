599b78c68ef5de63cf5ddfe317e2c478823754a1
stianst <stianst@gmail.com>
2024-02-14 11:23:06 +0100

    Support state reason for issues
    Closes #1792

---
+++ src/main/java/org/kohsuke/github/GHIssueStateReason.java
@@ -0,0 +1,13 @@
+package org.kohsuke.github;
+
+public enum GHIssueStateReason {
+
+    /** Completed **/
+    COMPLETED,
+
+    /** Closed as not planned **/
+    NOT_PLANNED,
+
+    /** Uknown **/
+    UNKNOWN
+}

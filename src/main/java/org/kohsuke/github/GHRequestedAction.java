4bdf1fe8b01e8d3e3d029650bb34e7dc683ef320
Yoann Rodière <yoann@hibernate.org>
2024-03-21 16:46:28 +0100

    Fix visibility of getters in GHRequestedAction
    Co-Authored-By: Liam Newman <bitwiseman@gmail.com>

--- src/main/java/org/kohsuke/github/GHRequestedAction.java
+++ src/main/java/org/kohsuke/github/GHRequestedAction.java
@@ -33,7 +33,7 @@ public class GHRequestedAction extends GHObject {
      *
      * @return the identifier
      */
-    String getIdentifier() {
+    public String getIdentifier() {
         return identifier;
     }
 

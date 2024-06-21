a899a65bcaad87d152bf99029a194f29202a5e3a
D061587 <benjamin.ihrig@sap.com>
2024-06-21 11:59:22 +0200

    Fix link tags

--- src/main/java/org/kohsuke/github/GHRepository.java
+++ src/main/java/org/kohsuke/github/GHRepository.java
@@ -3661,7 +3661,7 @@ public class GHRepository extends GHObject {
      * @return list of top referral paths
      * @throws IOException
      *             the io exception
-     * @link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-paths
+     * {@link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-paths}
      */
     public List<GHRepositoryTrafficTopReferralPath> getTopReferralPaths() throws IOException {
         return root().createRequest()
@@ -3677,7 +3677,7 @@ public class GHRepository extends GHObject {
      * @return list of top referrers
      * @throws IOException
      *             the io exception
-     * @link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-sources
+     * {@link https://docs.github.com/en/rest/metrics/traffic?apiVersion=2022-11-28#get-top-referral-sources}
      */
     public List<GHRepositoryTrafficTopReferralSources> getTopReferralSources() throws IOException {
         return root().createRequest()

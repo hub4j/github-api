f98355f1ee5bec08e46e8f7697900ce235114779
Valentin Delaye <jonesbusy@gmail.com>
2024-07-28 11:17:01 +0200

    Add missing javadoc

--- src/main/java/org/kohsuke/github/GHBranchSync.java
+++ src/main/java/org/kohsuke/github/GHBranchSync.java
@@ -2,14 +2,29 @@ package org.kohsuke.github;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
+/**
+ * The type Gh branch sync.
+ */
 public class GHBranchSync extends GitHubInteractiveObject {
 
+    /**
+     * The Repository that this branch is in.
+     */
     private GHRepository owner;
 
+    /**
+     * The message.
+     */
     private String message;
 
+    /**
+     * The merge type.
+     */
     private String mergeType;
 
+    /**
+     * The base branch.
+     */
     private String baseBranch;
 
     /**
@@ -22,18 +37,38 @@ public class GHBranchSync extends GitHubInteractiveObject {
         return owner;
     }
 
+    /**
+     * Gets message.
+     *
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Gets merge type.
+     *
+     * @return the merge type
+     */
     public String getMergeType() {
         return mergeType;
     }
 
+    /**
+     * Gets base branch.
+     *
+     * @return the base branch
+     */
     public String getBaseBranch() {
         return baseBranch;
     }
 
+    /**
+     * To string.
+     *
+     * @return the string
+     */
     @Override
     public String toString() {
         return "GHBranchSync{" + "message='" + message + '\'' + ", mergeType='" + mergeType + '\'' + ", baseBranch='"

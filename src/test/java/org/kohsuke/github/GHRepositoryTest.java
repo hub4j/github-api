50680f669cc5f7c975410dab7da6bb6609b4a42e
August Detlefsen <adetlefsen@flexport.com>
2024-08-09 14:03:48 -0700

    Add test

--- src/test/java/org/kohsuke/github/GHRepositoryTest.java
+++ src/test/java/org/kohsuke/github/GHRepositoryTest.java
@@ -502,6 +502,30 @@ public class GHRepositoryTest extends AbstractGitHubWireMockTest {
     }
 
     /**
+     * List contributors.
+     *
+     * @throws IOException
+     *             Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void listContributorsAnon() throws IOException {
+        GHRepository r = gitHub.getOrganization("hub4j").getRepository("github-api");
+        int i = 0;
+        boolean kohsuke = false;
+
+        for (GHRepository.Contributor c : r.listContributors(true)) {
+            if (c.getLogin().equals("kohsuke")) {
+                assertThat(c.getContributions(), greaterThan(0));
+                kohsuke = true;
+            }
+            if (i++ > 5) {
+                break;
+            }
+        }
+
+        assertThat(kohsuke, is(true));
+    }
+    /**
      * Gets the permission.
      *
      * @throws Exception

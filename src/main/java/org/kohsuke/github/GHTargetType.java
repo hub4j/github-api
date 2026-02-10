58d7468b41dc22538e231883c850eef61bca475a
rball11 <ravikumar.balla3@aexp.com>
2026-02-10 14:58:14 -0700

    spotless fixes

--- src/main/java/org/kohsuke/github/GHTargetType.java
+++ src/main/java/org/kohsuke/github/GHTargetType.java
@@ -13,10 +13,10 @@ import java.util.Locale;
  */
 public enum GHTargetType {
 
-    /** The organization. */
-    ORGANIZATION,
     /** The enterprise. */
     ENTERPRISE,
+    /** The organization. */
+    ORGANIZATION,
     /** The user. */
     USER;
 
